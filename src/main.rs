6d8231b9e82b429f8e629ed75112eac40d7d55d6
killuazoldyck680 <killuaz2049@gmail.com>
2026-06-09 02:07:54 +0530

    feat: initialize url shortener repository with foundational axum server layout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+
+
+
+
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(home_page));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Server running up at http://{}", addr);
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn home_page() {
+    println!("Welcome to my solo URL Shortener API!")
 }
