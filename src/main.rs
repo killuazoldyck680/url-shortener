3446a5f02e5c143731290c30204313412c12bf9c
killuazoldyck680 <killuaz2049@gmail.com>
2026-06-10 20:49:16 +0530

    feat: implement thread-safe AppState storage and define JSON data models

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
 use axum::{Router, routing::get};
 use std::net::SocketAddr;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use serde::{Deserialize, Serialize};
 
+type AppState = Arc<Mutex<HashMap<String, String>>>;
 
+#[derive(Deserialize)]
+struct ShortenRequest {
+    long_url: String,
+}
+
+#[derive(Serialize)]
+struct ShortenResponse {
+    short_url: String,
+}
 
 
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(home_page));
+
+    let shared_state: AppState = Arc::new(Mutex::new(HashMap::new()));
+
+
+    let app = Router::new().route("/", get(home_page))
+    .with_state(shared_state);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Server running up at http://{}", addr);
